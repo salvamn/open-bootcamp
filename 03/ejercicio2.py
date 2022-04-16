e7bbb1211f97032ab7f52fcf39c2fe4831d71c12
salva <salvador0796@gmail.com>
2022-04-15 23:07:56 -0400

    ejercicio2 de "control del flujo"

---
+++ 03/ejercicio2.py
@@ -0,0 +1,8 @@
+numero_inicial = 1
+numero_final = 20
+
+for numero in range(numero_inicial, numero_final + 1):
+    if numero % 2 == 0:
+        pass
+    else:
+        print(f"El numero: {numero} es impar")
