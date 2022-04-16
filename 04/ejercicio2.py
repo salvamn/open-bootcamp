5f80a284e4b2817ad7ed4b8832f199f99ef6c5e9
salva <salvador0796@gmail.com>
2022-04-15 23:53:31 -0400

    ejercicio2 de "funciones"

---
+++ 04/ejercicio2.py
@@ -0,0 +1,8 @@
+def primo_o_impar(numero):
+    if numero % 2 == 0:
+        print(f"El {numero} es par")
+    else:
+        print(f"El {numero} es impar")
+        
+primo_o_impar(2)
+primo_o_impar(21)
